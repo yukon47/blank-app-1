2265fcc7540608fbb1504a7031622c47727d949b
yukon47 <72011444+yukon47@users.noreply.github.com>
2025-10-27 11:46:07 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,5 @@ import streamlit as st
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-
     st.write("最初の第一歩")
 )
